a79553f81f515ca2b7032a9c3fc52b2388ac703a
Abdolkarim Saeedi <parsa592323@gmail.com>
2019-11-11 10:57:12 +0000

    Update InRadius.m

--- InRadius.m
+++ InRadius.m
@@ -22,8 +22,12 @@ And computing the InRadius size for each triangle.
 
 
 
+
 function [out radchng r] = InRadius(time_series, lag)
-% lag should be chosen based on sampling rate and time_series data Typical values are 1:5 / higher than 6 lags will have data loss
+% lag should be chosen based on sampling rate and time_series data Typical values are 1:5 / higher than 6 lags will have data loss.
+
+	% Parameter control.
+	
 	if nargin<2 || nargin>2 
 		fprintf('WARNING: 2 input arguments: time_series and lag are needed.\n refer to InRadius comments for more information.\n')
 	end
@@ -33,25 +37,34 @@ function [out radchng r] = InRadius(time_series, lag)
 	if lag>length(time_series)
 		error('lag must be smaller than data size.')
 	end
-	% Looping through all the samples
+	% Looping through all the samples.
+	
 	for i=1:length(time_series)-5
- 		% Triangle area calculation for six consecutive samples
- 		% With D representing the area for each triangle
+ 		% Triangle area calculation for six consecutive samples.
+ 		% With D representing the area for each triangle.
+		
 		D(i)=polyarea([time_series(i),time_series(i+2),time_series(i+4)],[time_series(i+1),time_series(i+3),time_series(i+5)]);
-		% Triangle side calculation
-		% With x, y, z denoting each triangle side
+		% Triangle side calculation.
+		% With x, y, z denoting each triangle side.
+		
 		x(i)=sqrt((time_series(i)-time_series(i+2))^2+(time_series(i+1)-time_series(i+3))^2);
 		y(i)=sqrt((time_series(i)-time_series(i+4))^2+(time_series(i+1)-time_series(i+5))^2);
 		z(i)=sqrt((time_series(i+2)-time_series(i+4))^2+(time_series(i+3)-time_series(i+5))^2);
-			% Inradius computation for every triangle
+			% Inradius computation for every triangle.
+			% A bug where triangle sides might end up being zero.
+			
 			if x(i)+y(i)+z(i)==0
 				r(i)=0;
 			else
+			
+			% Incenter Radius calculation with one of the popular formulas.
+			
 				r(i)=2*(D(i))/(x(i)+y(i)+z(i));
 			end
 	end
 % Any analysis can be done with the resulting signal here.
 % Absolute Derivative calculation for the InRadius sequence.
+
 radchng=abs(r(1:lag:end-1)-r(2:lag:end));
 out=mean(radchng);
 end
