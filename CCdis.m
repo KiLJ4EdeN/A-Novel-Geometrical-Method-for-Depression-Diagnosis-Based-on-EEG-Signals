2816dda38385b61ac145ef1d7d83e83809d57f6a
Abdolkarim Saeedi <parsa592323@gmail.com>
2019-11-11 10:33:44 +0000

    Update CCdis.m

--- CCdis.m
+++ CCdis.m
@@ -28,6 +28,7 @@ function [out D]=CCdis(time_series, lag)
     % looping the signal to exctract centroids.
     for k=1:length(time_series)-5
     		% calculating the centroid coordinates for every triangle.
+		% with cx and cy representing the centroids x and y coords respectiely.
 		cx(k)=mean([time_series(k),time_series(k+2),time_series(k+4)]);
 		cy(k)=mean([time_series(k+1),time_series(k+3),time_series(k+5)]);
     end
@@ -35,9 +36,11 @@ function [out D]=CCdis(time_series, lag)
     cx=cx(1:lag:end);
     cy=cy(1:lag:end);
     % measuring the distance between two consecutive triangle centers.
+    % note: any type of desire distance can be measured
+    % euclidean chebychev mikowski cityblock etc.
     D=sqrt((cx(2:end)-cx(1:end-1)).^2+(cy(2:end)-cy(1:end-1)).^2);
     % the triangle distance vector can be used to your needs.
     % here the mean is calculated as a feature.
-    out=mean(abs(aji(1:lag:end-1)-aji(2:lag:end)));
+    out=mean(abs(D(1:lag:end-1)-D(2:lag:end)));
 end
     
