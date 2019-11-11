5748603fe8f85ddee87a82a89de0f8b7740fef9d
Abdolkarim Saeedi <parsa592323@gmail.com>
2019-11-11 10:53:57 +0000

    Update CCdis.m

--- CCdis.m
+++ CCdis.m
@@ -25,7 +25,9 @@ And computing the distances between each pair of Centroids.
 
 
 function [out D]=CCdis(time_series, lag)
-    % looping the signal to exctract centroids.
+
+	% Parameter Control
+    
     	if nargin<2 || nargin>2 
 		fprintf('WARNING: 2 input arguments: time_series and lag are needed.\n refer to InRadius comments for more information.\n')
 	end
@@ -35,21 +37,27 @@ function [out D]=CCdis(time_series, lag)
 	if lag>length(time_series)
 		error('lag must be smaller than data size.')
 	end
+	    % Looping the signal samples to exctract centroids.
+	    
 	    for k=1:length(time_series)-5
 			% calculating the centroid coordinates for every triangle.
 			% with cx and cy representing the centroids x and y coords respectiely.
+			
 			cx(k)=mean([time_series(k),time_series(k+2),time_series(k+4)]);
 			cy(k)=mean([time_series(k+1),time_series(k+3),time_series(k+5)]);
 	    end
 	    % extracting the triangle centers that meet the lag criteria.
+	    
 	    cx=cx(1:lag:end);
 	    cy=cy(1:lag:end);
 	    % measuring the distance between two consecutive triangle centers.
 	    % note: any type of desire distance can be measured
 	    % euclidean chebychev mikowski cityblock etc.
+	    
 	    D=sqrt((cx(2:end)-cx(1:end-1)).^2+(cy(2:end)-cy(1:end-1)).^2);
 	    % the triangle distance vector can be used to your needs.
 	    % here the mean is calculated as a feature.
+	    
 	    out=mean(abs(D(1:lag:end-1)-D(2:lag:end)));
 end
     
