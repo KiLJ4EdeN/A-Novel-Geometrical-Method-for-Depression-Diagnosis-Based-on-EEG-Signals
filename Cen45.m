4e7531003a9c30ad416f83b61880d682a0f316c6
Abdolkarim Saeedi <parsa592323@gmail.com>
2019-11-24 13:13:36 +0330

    Update Cen45.m

--- Cen45.m
+++ Cen45.m
@@ -18,8 +18,8 @@ And computing the the shortest distances between each triangle center and the x=
  [out Censeq] = Cen45(time_series, 1);
  plot(D)
  out
- note that this results in a differentiation from the average of the in distances between centroids.
- to use the Centroid distance variability sequence a second output D must be specified.
+ note that this results in a differentiation from the average of the in distances between centroids and the 45-degree line.
+ to use the Centroid distance to 45-degree line variability sequence a second output D must be specified.
 %}
 
 
@@ -32,9 +32,9 @@ function [out Censeq] = Cen45(time_series, lag)
 
         % Parameter Control
     
-    	   if nargin < 2 || nargin > 2 
-		              fprintf('WARNING: 2 input arguments: time_series and lag are needed.\n refer to InRadius comments for more information.\n')
-	       end
+    	if nargin < 2 || nargin > 2
+		fprintf('WARNING: 2 input arguments: time_series and lag are needed.\n refer to InRadius comments for more information.\n')
+	end
         if ~ isvector(time_series)
                 error('time_series must be a 1*N vector.')
         end
@@ -47,8 +47,8 @@ function [out Censeq] = Cen45(time_series, lag)
            		       	% calculating the centroid coordinates for every triangle.
 			                  % with cx and cy representing the centroids x and y coords respectiely.
                      
-                     cx(k) = mean([ld3(k),ld3(k+2),ld3(k+4)]);
-                     cy(k) = mean([ld3(k+1),ld3(k+3),ld3(k+5)]);
+                     cx(k) = mean([time_series(k), time_series(k+2), time_series(k+4)]);
+                     cy(k) = mean([time_series(k+1), time_series(k+3), time_series(k+5)]);
             
            end
            
@@ -58,5 +58,5 @@ function [out Censeq] = Cen45(time_series, lag)
            % measuring the shortest distance between the triangle centroid and the x=y line.
            D(k) = (abs(cx(k) - cy(k)))/sqrt(2);
        
-           out = mean(abs(abi(1:end-5) - abi(6:end)));
+           out = mean(abs(D(1:end-5) - D(6:end)));
 end
