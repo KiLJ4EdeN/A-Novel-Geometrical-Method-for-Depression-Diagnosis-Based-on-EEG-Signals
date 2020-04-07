1c7a58826d8a5ca6e4b20dbd87bcd79f8e143f53
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-04-07 07:47:26 -0300

    Create CCdis_python.py

---
+++ CCdis_python.py
@@ -0,0 +1,17 @@
+# python implementation of CCdis
+# please kindly cite the article if you find these methods useful to your application.
+def CCdis(arr, lag=1):
+  N = len(arr) - 5
+  cx = np.zeros(N)
+  cy = np.zeros(N)
+  i = 0
+  while i <= N:
+    print(i)
+    cx[i] = np.mean([arr[i], arr[i+1], arr[i+2]])
+    cy[i] = np.mean([arr[i+1], arr[i+3], arr[i+5]])
+    i += lag
+  CCseq = np.sqrt((cx[2:]-cx[1:-1])**2 + (cy[2:]-cy[1:-1])**2);
+  output = np.mean(CCseq)
+  return CCseq, output
+
+CCseq, output = CCdis(y, 12)
